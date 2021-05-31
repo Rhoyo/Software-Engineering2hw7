370cccdfa4a07d0dbfde8324f83a67a0ea7cbbfb
Ryan Ho <hor@flip3.engr.oregonstate.edu>
2021-05-31 13:52:43 -0700

    Wrote 2nd test, tests if value is divisible by 5 but not 3

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,7 +1,9 @@
 def fizzBuzz(a):
 	if(a%3==0):
 		return 0
-	else
+#	elif(a%5==0):
+#		return 2 
+	else:
 		return 1
 
 
--- test_FizzTDD.py
+++ test_FizzTDD.py
@@ -5,3 +5,8 @@ class testFizz(unittest.TestCase):
 	def test_fizz(self):
 		res=fizzBuzz.fizzBuzz(3)
 		self.assertEqual(res, 0)
+	
+	def test_buzz(self):
+		res=fizzBuzz.fizzBuzz(5)
+		self.assertEqual(res, 2)
+
