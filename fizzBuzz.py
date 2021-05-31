1dbfe0d5e16fad03a6dd6a244af881457681edca
Ryan Ho <hor@flip3.engr.oregonstate.edu>
2021-05-31 14:05:25 -0700

    Implemented printOut method, prints out Xs current value, fizz, buzz , or fizzBuzz depending if it divisible by 3,5 or both

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -8,4 +8,20 @@ def fizzBuzz(a):
 	else:
 		return 1
 
-
+def printOut():
+	x=1
+	f="fizz"
+	b="buzz"
+	fb="fizzBuzz"
+	for x in range(1,100):
+		if(fizzBuzz(x)==1):
+			print(x)
+		elif(fizzBuzz(x)==0):
+			print(f) 
+		elif(fizzBuzz(x)==2):
+			print(b)
+ 		elif(fizzBuzz(x)==3):
+			print(fb)
+			
+			
+printOut() 
